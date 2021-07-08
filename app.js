ba9b749ce99e9d1f40db9e0b4ab67d986f9cd558
Oğuzhan Filiz <oguzhan.filiz@ogr.dpu.edu.tr>
2021-07-08 14:15:19 +0300

    Vue tanımlaması ve methodların oluşturulması

---
+++ app.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: "#app",
+    data: {
+        player_heal: 100,
+        monster_heal: 100,
+        game_is_on: false
+    },
+    methods: {
+        start_game: function () {
+            this.game_is_on = true
+        },
+        attach: function () {
+
+        }, 
+        special_attach: function () {
+
+        },
+        first_aid: function () {
+
+        },
+        give_up: function () {
+
+        }
+    }
+})
